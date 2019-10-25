5d4261b1450315ce2ac707ad4dd60628496739dc
changuitoss <ezealvarezrubio@gmail.com>
2019-10-25 18:15:55 -0300

    No me acuerdo qué cambié

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,20 @@
-const posibilidades = ['rock', 'scissors', 'paper', 'rock', 'scissors'];  // ordenadas de forma que siempre [i] le gana a [i + 1]
+const buttons = document.querySelectorAll('button');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const pW = document.querySelector('#win');
+const pT = document.querySelector('#tie');
+const pL = document.querySelector('#loose');
+const pc = document.querySelector('#pc');
+const round = document.querySelector('#round');
+const ganador = document.querySelector('#ganador');
+
+const posibilidades = ['Piedra', 'Tijera', 'Papel', 'Piedra', 'Tijera'];  // ordenadas de forma que siempre [i] le gana a [i + 1]
 
 var puntajePlayer = 0;
 var puntajeAi = 0;
 var empate = 0;
 var roundsTotal = 0;
-
-const buttons = document.querySelectorAll('button');
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
 
 var computerSelection;
 var playerSelection;
@@ -29,39 +35,40 @@ const computerPlay = () => {
 	return posibilidades[azar];
 }
 
-const mensajes = (roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate) => {
-												console.log('Round: ', roundsTotal);
-												console.log('------------');
-												console.log('Tu eleccion: ', playerSelection);
-												console.log('PC eleccion: ', computerSelection);
-												console.log('------------');
-												console.log('Vos:', puntajePlayer)
-												console.log('PC: ', puntajeAi)
-												console.log('Empates: ', empate)
-												console.log('**********************************');
+const mensajes = (roundsTotal, computerSelection, puntajePlayer, puntajeAi, empate) => {
+					round.textContent = `Round: ${roundsTotal}`;
+					pc.textContent = `Elección de la PC: ${computerSelection}`;
+					pW.textContent = `Ganados: ${puntajePlayer}`;
+					pT.textContent = `Empates: ${empate}`;
+					pL.textContent = `Perdidos: ${puntajeAi}`;
 }
 
 
-
-
 const plays = (playerSelection, computerSelection) => {
-	roundsTotal += 1;
+	roundsTotal++;
 
-	if (playerSelection == posibilidades[0] || playerSelection == posibilidades[1] || playerSelection == posibilidades[2]) {
+	if (playerSelection == 'piedra' || playerSelection == 'papel' || playerSelection == 'tijera') {
 		if (computerSelection == posibilidades[posibilidades.indexOf(playerSelection) + 1]) {  // chequea que la eleccion de la compu sea el proximo en posibilidades[]
 			puntajePlayer += 1;
-			return mensajes(roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate);
+			mensajes(roundsTotal, computerSelection, puntajePlayer, puntajeAi, empate)
 		} else if(computerSelection == posibilidades[posibilidades.indexOf(playerSelection) + 2]) {  // chequea que la eleccion de la compu sea el [i + 2] en posibilidades[]
 				puntajeAi += 1;
-				return mensajes(roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate);
+				mensajes(roundsTotal, computerSelection, puntajePlayer, puntajeAi, empate)
 		} else {
 				empate += 1;
-				return mensajes(roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate);
-				 
-		}
+				mensajes(roundsTotal, computerSelection, puntajePlayer, puntajeAi, empate)
+			}
 	}
 
-
-	
-
+	if (puntajePlayer > puntajeAi && puntajePlayer == 5) {
+		ganador.textContent = 'Ganaste!!!'
+		
+		buttons.removeEventListener('click', (e) => {
+			var playerSelection = e.target.value;
+			var computerSelection = computerPlay();
+			plays(playerSelection, computerSelection);
+		})
+	} else if (puntajePlayer < puntajeAi && puntajeAi == 5) {
+			ganador.textContent = 'Perdiste!!!'
+	}
 }
