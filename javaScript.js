d8649ec1534a172482239c37a74beac379f8e43b
changuitoss <ezealvarezrubio@gmail.com>
2019-10-25 13:19:42 -0300

    Botones, win/lose, todo en console: OK!

--- javaScript.js
+++ javaScript.js
@@ -10,54 +10,58 @@ const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 
-buttons.forEach((button) => {
-	button.addEventListener('click', () => {
-	alert('test listener');
-	})
-});
-
 var computerSelection;
 var playerSelection;
 
+buttons.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		var playerSelection = e.target.value;
 
-const game = () => {
-	for(var i = 0; i < 5; i++) {  // se juegan 3 rounds
-		roundsTotal += 1;
+		var computerSelection = computerPlay();
+
 		plays(playerSelection, computerSelection);
-		console.log('------------');
-		console.log('Vos:', puntajePlayer)
-		console.log('PC: ', puntajeAi)
-		console.log('Empates: ', empate)
-		console.log('**********************************');
-	}
-}
+		})
+});
+
 
 const computerPlay = () => {
 	var azar = Math.floor(Math.random() * 3)
 	return posibilidades[azar];
 }
 
-
-const plays = (playerSelection, computerSelection) => {
-	var i = 0;
-	console.log('Round: ', roundsTotal);
-	console.log('------------');
-	//playerSelection = prompt('Elegi: Rock, Paper o Scissors').toLowerCase(); // TODO: no permitir que se ingrese cualquier cosa-.
-	console.log('Tu eleccion: ', playerSelection);
-
-	var computerSelection = computerPlay();
-	console.log('PC eleccion: ', computerSelection);
-
-	if (playerSelection == posibilidades[i] || playerSelection == posibilidades[i + 1] || playerSelection == posibilidades[i + 2]) {
-		if (computerSelection == posibilidades[posibilidades.indexOf(playerSelection) + 1]) {  // chequea que la eleccion de la compu sea el proximo en posibilidades[]
-			return puntajePlayer += 1;
-		} else if(computerSelection == posibilidades[posibilidades.indexOf(playerSelection) + 2]) {  // chequea que la eleccion de la compu sea el [i + 2] en posibilidades[]
-			return puntajeAi += 1;
-		} else {
-		return empate += 1;
-		}
-	}
+const mensajes = (roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate) => {
+												console.log('Round: ', roundsTotal);
+												console.log('------------');
+												console.log('Tu eleccion: ', playerSelection);
+												console.log('PC eleccion: ', computerSelection);
+												console.log('------------');
+												console.log('Vos:', puntajePlayer)
+												console.log('PC: ', puntajeAi)
+												console.log('Empates: ', empate)
+												console.log('**********************************');
 }
 
 
-game();
+
+
+const plays = (playerSelection, computerSelection) => {
+	roundsTotal += 1;
+
+	if (playerSelection == posibilidades[0] || playerSelection == posibilidades[1] || playerSelection == posibilidades[2]) {
+		if (computerSelection == posibilidades[posibilidades.indexOf(playerSelection) + 1]) {  // chequea que la eleccion de la compu sea el proximo en posibilidades[]
+			puntajePlayer += 1;
+			return mensajes(roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate);
+		} else if(computerSelection == posibilidades[posibilidades.indexOf(playerSelection) + 2]) {  // chequea que la eleccion de la compu sea el [i + 2] en posibilidades[]
+				puntajeAi += 1;
+				return mensajes(roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate);
+		} else {
+				empate += 1;
+				return mensajes(roundsTotal, playerSelection, computerSelection, puntajePlayer, puntajeAi, empate);
+				 
+		}
+	}
+
+
+	
+
+}
